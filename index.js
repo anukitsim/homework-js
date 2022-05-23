09f7ed93ae2db6636bdbe5f049073e8b6bc42b8a
anukitsim <104069419+anukitsim@users.noreply.github.com>
2022-05-24 02:46:46 +0400

    done.

---
+++ index.js
@@ -0,0 +1,24 @@
+var salary = prompt ("enter your salary");
+var isAvarageSalary = '';
+
+if (salary <= 800) {
+    isAvarageSalary = "avarage";
+}
+else {
+    isAvarageSalary = "high salary";
+}
+
+console.log('your salary is:' + isAvarageSalary);
+
+alert('your salary is:' + isAvarageSalary);
+
+
+
+const arr = [
+    ['Ann', 22, false],
+    ['Bill', 40, true],
+    ['Jack', 19, true]
+];
+
+console.log(arr[0][2] + '' , + arr[1][1] + '' , '' + arr[2][2])
+
